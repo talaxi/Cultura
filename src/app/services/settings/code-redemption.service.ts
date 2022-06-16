d55755ca6c6e930e3d41fe1460026aa544677bc1
Talaxi <jaredbeaudrot@gmail.com>
2022-06-16 13:08:25 -0400

    testing

--- src/app/services/settings/code-redemption.service.ts
+++ src/app/services/settings/code-redemption.service.ts
@@ -3,6 +3,7 @@ import { ResourceValue } from 'src/app/models/resources/resource-value.model';
 import { ShopItemTypeEnum } from 'src/app/models/shop-item-type-enum.model';
 import { environment } from '../../../environments/environment';
 import * as CryptoJS from 'crypto-js';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable({
   providedIn: 'root'
@@ -26,9 +27,12 @@ export class CodeRedemptionService {
   redeemCode(encryptedVal: string) {
     var key = environment.CODEREDEMPTIONSECRET;
     var decrypted = CryptoJS.AES.decrypt(encryptedVal, key);
-    
-    var loadDataJson = <ResourceValue[]>JSON.parse(decrypted.toString());
+    console.log("Decrypted: " );
+    console.log(decrypted.toString(CryptoJS.enc.Utf8));
+
+    var parsedRewards = <ResourceValue[]>JSON.parse(decrypted.toString());
+    //plainToInstance(GlobalVariables, loadDataJson);
       //this.globalService.globalVar = plainToInstance(GlobalVariables, loadDataJson);
-    console.log(loadDataJson);
+    console.log(parsedRewards);    
   }
 }
