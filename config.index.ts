e994947747613928e37bbaacf960396695bc699f
Talaxi <jaredbeaudrot@gmail.com>
2022-06-15 23:17:22 -0400

    secret test

--- config.index.ts
+++ config.index.ts
@@ -6,7 +6,7 @@ const targetPath = 'src/environments/environment.prod.ts';
 
 const envConfigFile = `export const environment = {
     production: true,
-    CODEREDEMPTIONSECRET: '${process.env.CODEREDEMPTIONSECRETVALUE}'  
+    CODEREDEMPTIONSECRET: 'ProdTest'  
 };
 `;
 
