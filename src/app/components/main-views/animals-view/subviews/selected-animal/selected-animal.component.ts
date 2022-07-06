c992d22c9b9b8dbb669dc444ab55fe2ae28b45e5
Talaxi <jaredbeaudrot@gmail.com>
2022-07-06 19:38:57 -0400

    turn talents off

--- src/app/components/main-views/animals-view/subviews/selected-animal/selected-animal.component.ts
+++ src/app/components/main-views/animals-view/subviews/selected-animal/selected-animal.component.ts
@@ -64,7 +64,7 @@ export class SelectedAnimalComponent implements OnInit {
   orbCells: ResourceValue[]; //for display purposes
   selectedOrb: ResourceValue;
 
-  talentsReleased = true;
+  talentsReleased = false;
   talentTreeOptions: string[];
   selectedTalentTree: string;
   talentTreeDescription: string;
